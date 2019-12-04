5b2882a2c8a7030217cfd96fc6c6115eee4cefdf
Bob White <robert.wd.white@gmail.com>
2019-12-03 21:42:05 -0800

    Day 04
    Still kind of gross, but I just love list comprehensions way more.
    Its a sickness.

--- rwhite_04.py
+++ rwhite_04.py
@@ -1,16 +1,9 @@
 from string import digits
-password_range = range(138241, 674034)
-possible_passwords = []
-for val in password_range:
-    pword = str(val)
-    if ''.join(sorted(pword)) == pword:
-        if any(pword[i] == pword[i+1] for i in range(len(pword)-1)):
-        # if any(pword.count(d) > 1 for d in digits):
-            possible_passwords.append(pword)
-
+from typing import Iterable
+possible_passwords: Iterable[str] = (str(v) for v in range(138241, 674034))
+possible_passwords = [v for v in possible_passwords
+                        if ''.join(sorted(v)) == v
+                        and any(len(set(v[i:i+2])) == 1 for i in range(len(v)-1))]
+filtered_passwords = [v for v in possible_passwords if any(v.count(d) == 2 for d in digits)]
 print(len(possible_passwords))
-filtered_passwords = []
-for possible in possible_passwords:
-    if any(possible.count(d) == 2 for d in digits):
-        filtered_passwords.append(possible)
 print(len(filtered_passwords))
