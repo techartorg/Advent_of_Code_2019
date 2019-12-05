c1d6cad388a72b61caae0662beb33cbd2fae5461
Bob White <robert.wd.white@gmail.com>
2019-12-05 00:01:57 -0600

    Day 04
    Small tweak, for part I didn't need to do a rolling window.
    The values are already sorted, so duplicates have to be concurrent.

--- rwhite_04.py
+++ rwhite_04.py
@@ -3,7 +3,7 @@ from typing import Iterable
 possible_passwords: Iterable[str] = (str(v) for v in range(138241, 674034))
 possible_passwords = [v for v in possible_passwords
                         if ''.join(sorted(v)) == v
-                        and any(len(set(v[i:i+2])) == 1 for i in range(len(v)-1))]
+                        and any(v.count(d) > 1 for d in digits)]
 filtered_passwords = [v for v in possible_passwords if any(v.count(d) == 2 for d in digits)]
 print(len(possible_passwords))
 print(len(filtered_passwords))
