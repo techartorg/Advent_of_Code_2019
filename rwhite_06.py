7772448b1957aee48d66ba0359c22872cc69cdf4
Bob White <robert.wd.white@gmail.com>
2019-12-06 00:12:53 -0600

    Day 06
    Using a defaultdict as I like the look of just using append and extend.

--- rwhite_06.py
+++ rwhite_06.py
@@ -1,17 +1,17 @@
-from collections import deque
+from collections import deque, defaultdict
 from typing import Dict, List
-
 
 data = deque(open('day_06.input').read().split('\n'))
 
-orbital_tree: Dict[str, List[str]] = {'COM': []}
+orbital_tree: Dict[str, List[str]] = defaultdict(list)
+orbital_tree['COM'] = [] # Need to prime the tree with the base node
 while data:
     parent, child = data[-1].split(')')
     if parent not in orbital_tree:
         data.rotate(1)
         continue
 
-    orbital_tree[child] = [parent]
+    orbital_tree[child].append(parent)
     orbital_tree[child].extend(orbital_tree[parent])
     data.pop()
 
