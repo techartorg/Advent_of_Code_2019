dc77ea75877de990b90fe5587a85fbc7765d116a
Bob White <robert.wd.white@gmail.com>
2019-12-06 00:15:05 -0600

    Day 06
    Better usage of the deque interface.
    No longer rotating and popping at the end.
    Instead we just appendleft if the value isn't useful yet.

--- rwhite_06.py
+++ rwhite_06.py
@@ -1,19 +1,18 @@
 from collections import deque, defaultdict
 from typing import Dict, List
 
-data = deque(open('day_06.input').read().split('\n'))
+data = deque([v.split(')') for v in open('day_06.input').read().split('\n')])
 
 orbital_tree: Dict[str, List[str]] = defaultdict(list)
 orbital_tree['COM'] = [] # Need to prime the tree with the base node
 while data:
-    parent, child = data[-1].split(')')
+    parent, child = data.pop()
     if parent not in orbital_tree:
-        data.rotate(1)
+        data.appendleft([parent, child])
         continue
 
     orbital_tree[child].append(parent)
     orbital_tree[child].extend(orbital_tree[parent])
-    data.pop()
 
 print(sum(len(v) for v in orbital_tree.values()))
 print(len(set(orbital_tree['SAN']).symmetric_difference(orbital_tree['YOU'])))
