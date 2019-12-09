a204745489f152901e99821c5279078d9fd22dfe
Jeff Hanna <jeff.b.hanna@gmail.com>
2019-12-09 09:52:06 -0600

    Part 1 and 2 100%

--- jhanna_08.py
+++ jhanna_08.py
@@ -20,9 +20,81 @@ Layer 2: 789
 The image you received is 25 pixels wide and 6 pixels tall.
 
 To make sure the image wasn't corrupted during transmission, the Elves would like you to find the layer that contains the fewest 0 digits. On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
+
+--- Part Two ---
+Now you're ready to decode the image. The image is rendered by stacking the layers and aligning the pixels with the same positions in each layer. The digits indicate the color of the corresponding pixel: 0 is black, 1 is white, and 2 is transparent.
+
+The layers are rendered with the first layer in front and the last layer in back. So, if a given position has a transparent pixel in the first and second layers, a black pixel in the third layer, and a white pixel in the fourth layer, the final image would have a black pixel at that position.
+
+For example, given an image 2 pixels wide and 2 pixels tall, the image data 0222112222120000 corresponds to the following image layers:
+
+Layer 1: 02
+         22
+
+Layer 2: 11
+         22
+
+Layer 3: 22
+         12
+
+Layer 4: 00
+         00
+Then, the full image can be found by determining the top visible pixel in each position:
+
+The top-left pixel is black because the top layer is 0.
+The top-right pixel is white because the top layer is 2 (transparent), but the second layer is 1.
+The bottom-left pixel is white because the top two layers are 2, but the third layer is 1.
+The bottom-right pixel is black because the only visible pixel in that position is 0 (from layer 4).
+So, the final image looks like this:
+
+01
+10
+What message is produced after decoding your image?
 """
 
 from collections import Counter
+
+
+def decode_image( width: int, height: int, raw_data: str ) -> None:
+	"""
+	Decodes the raw data into an image using these rules:
+	* Layers are stacked back to front ( highest layer # -> lowest layer # )
+	* Pixel values are:
+		0 = black
+		1 = white
+		2 = transparent
+
+	Arguments:
+		 width {int} -- The pixel width of the image
+		 height {int} -- The pixel height of the image
+		 raw_data {str} -- The raw, non-delimited, pixel data.
+	"""
+
+	pixel_count: int = width * height
+	layers = [ ]
+
+	for i in range( 0, len( raw_data ), pixel_count ):
+		layers.append( [ int( x ) for x in raw_data[ i: i + pixel_count ] ] )
+
+	layers.reverse( )
+
+	colors = [ ]
+	for i in range( len( layers[ 0 ] ) ):
+		for j in range( len( layers ) ):
+			p = layers[ j ][ i ]
+
+			if p == 2:
+				continue
+			else:
+				p = ' ' if p == 0 else '#'
+				try:
+					colors[ i ] = p
+				except IndexError:
+					colors.append( p )
+
+	for i in range( 0, width * height, width ):
+		print( ''.join( colors[ i: i + width ] ) )
+
 
 def find_layer( width: int, height: int, raw_data: str ) -> int:
 	"""
@@ -41,10 +113,7 @@ def find_layer( width: int, height: int, raw_data: str ) -> int:
 	layers = [ ]
 
 	for i in range( 0, len( raw_data ), pixel_count ):
-		pixel_data = raw_data[ i: i + pixel_count ]
-
-		layer = Counter( pixel_data )
-		layers.append( layer )
+		layers.append( Counter( raw_data[ i: i + pixel_count ] ) )
 
 	lowest_0s_count = pixel_count + 1
 	lowest_0s_layer = None
@@ -64,3 +133,4 @@ if __name__ == '__main__':
 	layer_idx: int = find_layer( 25, 6, raw_data )
 
 	print( f'The 1s count multiplied by the 2s count on the layer with the lowest count of 0s is: {layer_idx}' )
+	decode_image( 25, 6, raw_data )
