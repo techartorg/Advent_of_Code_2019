77823ea0708e252f649488adb93114dc900e2502
Bob White <robert.wd.white@gmail.com>
2019-12-25 00:34:48 -0600

    Day 10
    Some variable name cleanups

--- rwhite_10.py
+++ rwhite_10.py
@@ -4,28 +4,27 @@ from typing import Dict, Tuple, Set, List
 
 AsteroidMap = Dict[Tuple[int, int], Set[Tuple[int, int]]]
 data = open('day_10.input').read().split()
-asteroids = [(j, i) for i in range(len(data)) for j in range(len(data[0])) if data[i][j] == '#']
+asteroids = [(x, y) for y in range(len(data)) for x in range(len(data[0])) if data[y][x] == '#']
 seen: AsteroidMap = defaultdict(set)
-for x, y in asteroids:
-    for a, b in asteroids:
-        if (x, y) == (a, b):
+for x0, y0 in asteroids:
+    for x1, x2 in asteroids:
+        if (x0, y0) == (x1, x2):
             continue
-        j, k = x-a, y-b
-        gcd = abs(math.gcd(j, k)) # get the greatest common demoninator
-        for m in range(1, gcd):
-            # Check for other asteroids along that vector
-            if any((a + j // gcd * m, b + k // gcd * m) in asteroids for m in range(1, gcd)):
-                break
-        else:
-            seen[x, y].add((a, b))
+        dx, dy = x0-x1, y0-x2
+        # get the greatest common demoninator
+        gcd = abs(math.gcd(dx, dy))
+        # Check for other asteroids along that vector
+        if not any((x1 + dx // gcd * m, x2 + dy // gcd * m) in asteroids for m in range(1, gcd)):
+            seen[x0, y0].add((x1, x2))
 
-best = max(seen, key=lambda x: len(seen[x]))
-print(best, len(seen[best]))
+
+best = max(seen, key=lambda x0: len(seen[x0]))
+print(len(seen[best]))
 
 lines: Dict[float, List[Tuple[int, int]]] = defaultdict(list)
-for x, y in [a for a in asteroids if a != best]:
+for x, y in [ast for ast in asteroids if ast != best]:
     angle = math.atan2((x-best[0]), (y-best[1])) - math.pi / 2 # Getting the angle offset from straight up
-    lines[angle].append((x,y))
+    lines[angle].append((x, y))
 
 for v in lines.values():
     v.sort(key=lambda x: abs(x[0]-best[0]) + abs(x[1]-best[1])) # Sorting based on distance from the start point
